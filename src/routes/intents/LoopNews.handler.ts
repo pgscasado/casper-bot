d4743e01775597853581efda57158bc53b2ea76a
Pedro Casado <37355453+pgscasado@users.noreply.github.com>
2021-08-03 14:17:24 -0300

    added chat loop and greeted difference

---
+++ src/routes/intents/LoopNews.handler.ts
@@ -0,0 +1,106 @@
+import { newsModel } from '../../models/News';
+import { IntentHandler } from '../../classes/Handler.class';
+
+interface FBGeneric {
+	image_url: string,
+	buttons: {
+		url: string,
+		type: "web_url",
+		title: string
+		}[],
+	title: string,
+	subtitle: string,
+	default_action: {
+		webview_height_ratio: "tall",
+		url: string,
+		type: "web_url"
+	}
+}
+
+export const loopNewsIntentHandler = new IntentHandler('noticias.loop - yes')
+	.default((intent, req, res) => {
+		if(['Esporte', 'Política', 'Entretenimento', 'Famosos'].includes(intent.parameters['noticias_categoria'])) {
+			newsModel.paginate({
+				query: { category: intent.parameters['noticias_categoria'] },
+				sort: { created_at: -1 },
+				page: req.query.page || 1,
+				limit: 10
+			}).then(
+				(docs) => { 
+					let elements: FBGeneric[] = []
+					if(docs && docs.docs && docs.docs.length > 0) {
+						docs.docs.forEach((news) => {
+							elements.push({
+								image_url: news.picture_url.valueOf(),
+								buttons: [
+									{
+										url: news.news_url.valueOf(),
+										type: "web_url",
+										title: "Visitar"
+									}
+								],
+								title: news.title.valueOf(),
+								subtitle: news.description.valueOf(),
+								default_action: {
+									webview_height_ratio: "tall",
+									url: news.news_url.valueOf(),
+									type: "web_url"
+								}
+							})
+						})
+						res.status(200).json({
+							"fulfillmentMessages": [{
+								"payload": {
+									"facebook": {
+										"attachment": {
+											"type": "template",
+											"payload": {
+											"template_type": "generic",
+											"elements": elements
+											}
+										}
+									}
+								},
+								"platform": "FACEBOOK"
+							}]
+						  })
+					} else {
+						res.status(200).json({
+							"fulfillmentMessages": [
+								{
+									"quickReplies": {
+										"title": "Me desculpe, eu não tenho nenhuma notícia para essa categoria :(\nVocê quer saber sobre outro tipo de notícia?",
+										"quickReplies": [
+											"Esportes",
+											"Política",
+											"Entretenimento",
+											"Famosos"
+										]
+									},
+									"platform": "FACEBOOK"
+								},
+							]
+						})
+					} },
+				(err) => { console.error(err.message); res.status(500).end(err.message); }
+			  )
+		} else {
+			console.error(`Unknown parameter ${intent.parameters['noticias_categoria']}`);
+			res.status(200).json({
+				"fulfillmentMessages": [
+					{
+						"quickReplies": {
+							"title": "Eu só posso te fornecer notícias sobre os seguintes temas: ",
+							"quickReplies": [
+								"Esportes",
+								"Política",
+								"Entretenimento",
+								"Famosos"
+							]
+						},
+						"platform": "FACEBOOK"
+					},
+				]
+			})
+		}
+	})
--- src/routes/intents/index.ts
+++ src/routes/intents/index.ts
@@ -1,9 +1,10 @@
 import { welcomeIntentHandler } from './Welcome.handler';
 import { IntentHandler } from '../../classes/Handler.class';
 import { newsIntentHandler } from './News.handler'
+import { loopNewsIntentHandler } from './LoopNews.handler';
 
 const handlers = [
-	newsIntentHandler, welcomeIntentHandler
+	newsIntentHandler, welcomeIntentHandler, loopNewsIntentHandler
 ]
 
 export const handlersMap: {[name: string]: IntentHandler} = {};
